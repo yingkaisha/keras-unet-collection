8830e8aafd362f32e4234fce4d56e9dcca40784a
Yingkai (Kyle) Sha <yingkai@eos.ubc.ca>
2021-02-05 10:52:48 -0800

    Update losses.py

--- keras_unet_collection/losses.py
+++ keras_unet_collection/losses.py
@@ -25,6 +25,7 @@ def _crps_tf(y_true, y_pred, factor=0.05):
 def crps2d_tf(y_true, y_pred, factor=0.05):
     
     '''
+    (Experimental)
     An approximated continuous ranked probability score (CRPS) loss function:
     
         CRPS = mean_abs_err - factor * std
@@ -43,6 +44,7 @@ def crps2d_tf(y_true, y_pred, factor=0.05):
         y_true: training target with shape=(batch_num, x, y, 1)
         y_pred: a forward pass with shape=(batch_num, x, y, 1)
         factor: relative importance of standard deviation term.
+        
     '''
     
     y_pred = tf.convert_to_tensor(y_pred)
@@ -75,6 +77,7 @@ def _crps_np(y_true, y_pred, factor=0.05):
 def crps2d_np(y_true, y_pred, factor=0.05):
     
     '''
+    (Experimental)
     Nunpy version of `crps2d_tf`.
     
     Documentation refers to `crps2d_tf`.
@@ -91,69 +94,115 @@ def crps2d_np(y_true, y_pred, factor=0.05):
         
     return crps_out/batch_num
 
-def triplet1d(y_true, y_pred, N, margin=5.0):
-    
+# ========================= #
+# Dice loss and variants
+
+def dice_coef(y_true, y_pred, const=K.epsilon()):
     '''
-    Semi-hard triplet loss with one-dimensional vectors of anchor, positive, and negative.
+    Sørensen–Dice coefficient for 2-d samples.
     
     Input
     ----------
-        y_true: a dummy input, not used within this function. Appear as a requirment of keras loss function format.
-        y_pred: a single pass of triplet training, with shape=(batch_num, 3*embeded_vector_size)
-                anchor, positive, and negative embeddings are expected to concatenate together.
-        N: Size (dimensions) of embedded vectors
-        margin: a positive number that prevents negative loss.
+        y_true, y_pred: predicted outputs and targets.
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
         
     '''
     
-    Embd_anchor = y_pred[:, 0:N]
-    Embd_pos = y_pred[:, N:2*N]
-    Embd_neg = y_pred[:, 2*N:]
-    
-    d_pos = tf.reduce_sum(tf.square(Embd_anchor - Embd_pos), 1)
-    d_neg = tf.reduce_sum(tf.square(Embd_anchor - Embd_neg), 1)
-    loss = tf.maximum(0., margin + d_pos - d_neg)
-    loss = tf.reduce_mean(loss)
-    
-    return loss
-
-def tversky_(y_true, y_pred, alpha=0.5):
-    
+    # flatten 2-d tensors
     y_true_pos = tf.reshape(y_true, [-1])
     y_pred_pos = tf.reshape(y_pred, [-1])
     
+    # get true pos (TP), false neg (FN), false pos (FP).
     true_pos  = tf.reduce_sum(y_true_pos * y_pred_pos)
     false_neg = tf.reduce_sum(y_true_pos * (1-y_pred_pos))
     false_pos = tf.reduce_sum((1-y_true_pos) * y_pred_pos)
     
-    return (true_pos + K.epsilon())/(true_pos + alpha*false_neg + (1-alpha)*false_pos + K.epsilon())
-
-def tversky(y_true, y_pred, alpha=0.5):
-    '''
-    Tversky Loss
+    # 2TP/(2TP+FP+FN) == 2TP/()
+    coef_val = (2.0 * true_pos + const)/(2.0 * true_pos + false_pos + false_neg)
     
-    tversky(y_true, y_pred, alpha=0.5)
+    return coef_val
+
+def dice(y_true, y_pred, const=K.epsilon()):
+    '''
+    Sørensen–Dice Loss.
+    
+    dice(y_true, y_pred, const=K.epsilon())
+    
+    Input
+    ----------
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
+    '''
+    # tf tensor casting
+    y_pred = tf.convert_to_tensor(y_pred)
+    y_true = tf.cast(y_true, y_pred.dtype)
+    
+    # <--- squeeze-out length-1 dimensions.
+    y_pred = tf.squeeze(y_pred)
+    y_true = tf.squeeze(y_true)
+    
+    loss_val = 1 - dice_coef(y_true, y_pred, const=const)
+    
+    return loss_val
+
+# ========================= #
+# Tversky loss and variants
+
+def tversky_coef(y_true, y_pred, alpha=0.5, const=K.epsilon()):
+    '''
+    Weighted Sørensen–Dice coefficient.
+    
+    Input
+    ----------
+        y_true, y_pred: predicted outputs and targets.
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
+    '''
+    
+    # flatten 2-d tensors
+    y_true_pos = tf.reshape(y_true, [-1])
+    y_pred_pos = tf.reshape(y_pred, [-1])
+    
+    # get true pos (TP), false neg (FN), false pos (FP).
+    true_pos  = tf.reduce_sum(y_true_pos * y_pred_pos)
+    false_neg = tf.reduce_sum(y_true_pos * (1-y_pred_pos))
+    false_pos = tf.reduce_sum((1-y_true_pos) * y_pred_pos)
+    
+    # TP/(TP + a*FN + b*FP); a+b = 1
+    coef_val = (true_pos + const)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + const)
+    
+    return coef_val
+
+def tversky(y_true, y_pred, alpha=0.5, const=K.epsilon()):
+    '''
+    Tversky Loss.
+    
+    tversky(y_true, y_pred, alpha=0.5, const=K.epsilon())
     
     ----------
     Hashemi, S.R., Salehi, S.S.M., Erdogmus, D., Prabhu, S.P., Warfield, S.K. and Gholipour, A., 2018. 
     Tversky as a loss function for highly unbalanced image segmentation using 3d fully convolutional deep networks. 
     arXiv preprint arXiv:1803.11078.
     
-    ----------
     Input
+    ----------
         alpha: tunable parameter within [0, 1]. Alpha handles imbalance classification cases.
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
     '''
-    
-    
+    # tf tensor casting
     y_pred = tf.convert_to_tensor(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
     
+    # <--- squeeze-out length-1 dimensions.
     y_pred = tf.squeeze(y_pred)
     y_true = tf.squeeze(y_true)
     
-    return 1 - tversky_(y_true, y_pred, alpha=alpha)
+    loss_val = 1 - tversky_coef(y_true, y_pred, alpha=alpha, const=const)
+    
+    return loss_val
 
-def focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3):
+def focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3, const=K.epsilon()):
     
     '''
     Focal Tversky Loss (FTL)
@@ -169,15 +218,52 @@ def focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3):
     Input
         alpha: tunable parameter within [0, 1]. Alpha handles imbalance classification cases 
         gamma: tunable parameter within [1, 3].
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
     '''
-    
+    # tf tensor casting
     y_pred = tf.convert_to_tensor(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
     
+    # <--- squeeze-out length-1 dimensions.
     y_pred = tf.squeeze(y_pred)
     y_true = tf.squeeze(y_true)
     
-    return tf.math.pow((1-tversky_(y_true, y_pred, alpha=alpha)), 1/gamma)
+    # (Tversky loss)**(1/gamma) 
+    loss_val = tf.math.pow((1-tversky_coef(y_true, y_pred, alpha=alpha, const=const)), 1/gamma)
+    
+    return loss_val
+# ========================= #
 
+def triplet_1d(y_true, y_pred, N, margin=5.0):
+    
+    '''
+    (Experimental)
+    Semi-hard triplet loss with one-dimensional vectors of anchor, positive, and negative.
+    
+    triplet_1d(y_true, y_pred, N, margin=5.0)
+    
+    Input
+    ----------
+        y_true: a dummy input, not used within this function. Appeared as a requirment of tf.keras.loss function format.
+        y_pred: a single pass of triplet training, with `shape=(batch_num, 3*embeded_vector_size)`.
+                i.e., `y_pred` is the ordered and concatenated anchor, positive, and negative embeddings.
+        N: Size (dimensions) of embedded vectors
+        margin: a positive number that prevents negative loss.
+        
+    '''
+    
+    # anchor sample pair separations.
+    Embd_anchor = y_pred[:, 0:N]
+    Embd_pos = y_pred[:, N:2*N]
+    Embd_neg = y_pred[:, 2*N:]
+    
+    # squared distance measures
+    d_pos = tf.reduce_sum(tf.square(Embd_anchor - Embd_pos), 1)
+    d_neg = tf.reduce_sum(tf.square(Embd_anchor - Embd_neg), 1)
+    loss_val = tf.maximum(0., margin + d_pos - d_neg)
+    loss_val = tf.reduce_mean(loss_val)
+    
+    return loss_val
 
 
