619620b1cf1f0d257436fe8c4a1559612af7da8f
Yingkai Sha <yingkaisha@gmail.com>
2021-02-28 11:19:10 -0700

    version 0.0.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "keras-unet-collection",
-    version = "0.0.16",
+    version = "0.0.17",
     author = "Yingkai (Kyle) Sha",
     author_email = "yingkaisha@gmail.com",
     description = "The Tensorflow, Keras implementation of U-net, V-net, U-net++, R2U-net, Attention U-net, ResUnet-a, U^2-Net, and UNET 3+ with optional ImageNet-trained backbones.",
