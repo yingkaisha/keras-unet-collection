ba2ae609e9f380928520ccd10dbf883fdd1cdb93
Yingkai Sha <yingkaisha@gmail.com>
2021-05-26 11:34:42 -0600

    adding backbone options for transUNET.

--- keras_unet_collection/_model_transunet_2d.py
+++ keras_unet_collection/_model_transunet_2d.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from keras_unet_collection.layer_utils import *
 from keras_unet_collection.activations import GELU, Snake
 from keras_unet_collection._model_unet_2d import UNET_left, UNET_right
+from keras_unet_collection._backbone_zoo import backbone_zoo, bach_norm_checker
 
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
@@ -135,8 +136,9 @@ def ViT_block(V, num_heads, key_dim, filter_num_MLP, activation='GELU', name='Vi
 
 
 def transunet_2d_base(input_tensor, filter_num, stack_num_down=2, stack_num_up=2, 
-                      proj_dim=768, num_mlp = 3072, num_heads=12, num_transformer=12,
-                      activation='ReLU', mlp_activation='GELU', batch_norm=False, pool=True, unpool=True, name='transunet'):
+                      proj_dim=768, num_mlp=3072, num_heads=12, num_transformer=12,
+                      activation='ReLU', mlp_activation='GELU', batch_norm=False, pool=True, unpool=True, 
+                      backbone=None, weights='imagenet', freeze_backbone=True, freeze_batch_norm=True, name='transunet'):
     '''
     The base of transUNET.
     
@@ -180,19 +182,56 @@ def transunet_2d_base(input_tensor, filter_num, stack_num_down=2, stack_num_up=2
     
     # ----- UNet-like downsampling ----- #
     
-    X = input_tensor
-    
-    # stacked conv2d before downsampling
-    X = CONV_stack(X, filter_num[0], stack_num=stack_num_down, activation=activation, 
-                   batch_norm=batch_norm, name='{}_down0'.format(name))
-    X_skip.append(X)
-    
-    for i, f in enumerate(filter_num[1:]):
-        X = UNET_left(X, f, stack_num=stack_num_down, activation=activation, pool=pool, 
-                      batch_norm=batch_norm, name='{}_down{}'.format(name, i+1))        
+    # no backbone cases
+    if backbone is None:
+
+        X = input_tensor
+
+        # stacked conv2d before downsampling
+        X = CONV_stack(X, filter_num[0], stack_num=stack_num_down, activation=activation, 
+                       batch_norm=batch_norm, name='{}_down0'.format(name))
         X_skip.append(X)
+
+        # downsampling blocks
+        for i, f in enumerate(filter_num[1:]):
+            X = UNET_left(X, f, stack_num=stack_num_down, activation=activation, pool=pool, 
+                          batch_norm=batch_norm, name='{}_down{}'.format(name, i+1))        
+            X_skip.append(X)
+
+    # backbone cases
+    else:
+        # handling VGG16 and VGG19 separately
+        if 'VGG' in backbone:
+            backbone_ = backbone_zoo(backbone, weights, input_tensor, depth_, freeze_backbone, freeze_batch_norm)
+            # collecting backbone feature maps
+            X_skip = backbone_([input_tensor,])
+            depth_encode = len(X_skip)
+            
+        # for other backbones
+        else:
+            backbone_ = backbone_zoo(backbone, weights, input_tensor, depth_-1, freeze_backbone, freeze_batch_norm)
+            # collecting backbone feature maps
+            X_skip = backbone_([input_tensor,])
+            depth_encode = len(X_skip) + 1
+
+
+        # extra conv2d blocks are applied
+        # if downsampling levels of a backbone < user-specified downsampling levels
+        if depth_encode < depth_:
+
+            # begins at the deepest available tensor  
+            X = X_skip[-1]
+
+            # extra downsamplings
+            for i in range(depth_-depth_encode):
+                i_real = i + depth_encode
+
+                X = UNET_left(X, filter_num[i_real], stack_num=stack_num_down, activation=activation, pool=pool, 
+                              batch_norm=batch_norm, name='{}_down{}'.format(name, i_real+1))
+                X_skip.append(X)
         
-    # discard the last tensor (will be replaced by the ViT output)
+    # subtrack the last tensor (will be replaced by the ViT output)
+    X = X_skip[-1]
     X_skip = X_skip[:-1] 
     
     # ----- ViT block after UNet-like encoding ----- #
@@ -236,12 +275,20 @@ def transunet_2d_base(input_tensor, filter_num, stack_num_down=2, stack_num_up=2
         X = UNET_right(X, [X_decode[i],], filter_num_decode[i], stack_num=stack_num_up, activation=activation, 
                        unpool=unpool, batch_norm=batch_norm, name='{}_up{}'.format(name, i))
         
+    # if tensors for concatenation is not enough
+    # then use upsampling without concatenation 
+    if depth_decode < depth_-1:
+        for i in range(depth_-depth_decode-1):
+            i_real = i + depth_decode
+            X = UNET_right(X, None, filter_num_decode[i_real], stack_num=stack_num_up, activation=activation, 
+                       unpool=unpool, batch_norm=batch_norm, concat=False, name='{}_up{}'.format(name, i_real)) 
+            
     return X
 
 def transunet_2d(input_size, filter_num, n_labels, stack_num_down=2, stack_num_up=2,
                  proj_dim=768, num_mlp = 3072, num_heads=12, num_transformer=12,
-                 activation='ReLU', mlp_activation='GELU', output_activation='Softmax', 
-                 batch_norm=False, pool=True, unpool=True, name='transunet'):
+                 activation='ReLU', mlp_activation='GELU', output_activation='Softmax', batch_norm=False, pool=True, unpool=True, 
+                 backbone=None, weights='imagenet', freeze_backbone=True, freeze_batch_norm=True, name='transunet'):
     '''
     transUNET
     
@@ -265,8 +312,8 @@ def transunet_2d(input_size, filter_num, n_labels, stack_num_down=2, stack_num_u
     # base    
     X = transunet_2d_base(IN, filter_num, stack_num_down=stack_num_down, stack_num_up=stack_num_up, 
                           proj_dim=proj_dim, num_mlp=num_mlp, num_heads=num_heads, num_transformer=num_transformer,
-                          activation=activation, mlp_activation=mlp_activation, batch_norm=batch_norm, 
-                          pool=pool, unpool=unpool, name=name)
+                          activation=activation, mlp_activation=mlp_activation, batch_norm=batch_norm, pool=pool, unpool=unpool,
+                          backbone=backbone, weights=weights, freeze_backbone=freeze_backbone, freeze_batch_norm=freeze_backbone, name=name)
     
     # output layer
     OUT = CONV_output(X, n_labels, kernel_size=1, activation=output_activation, name='{}_output'.format(name))
